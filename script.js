ad57d3ad1481eba37e3edcbffe07b5938b247050
Bharadwaja sahoo <bharadwajasahoo96@gmail.com>
2024-05-21 14:09:06 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $('.fa-bars').click(function(){
+        $(this).toggleClass('fa-times');
+        $('.navbar').toggleClass('nav-toggle');
+    });
+    $(Window).on('scroll load',function(){
+        $('.fa-bars').removeClass('fa-times');
+        $('.navbar').removeClass('nav-toggle');
+        
+        if($(Window).scrollTop()>30){
+            $('header').addClass('header-active');
+        }
+        else{
+            $('header').removeClass('header-active');
+        }
+    })
+});
